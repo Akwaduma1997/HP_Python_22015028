0ae4811fe21a77c0841b24129330423c965bb3e0
Akwaduma1997 <134207770+Akwaduma1997@users.noreply.github.com>
2023-06-22 11:44:07 +0100

    Create python_basic2.py

---
+++ python_basic2.py
@@ -0,0 +1,11 @@
+# Program to swap numbers a and b respectively
+print("Swapping Algorithm")
+a = 6
+b = 8
+print("The value in a =", a, "\nThe Value in b =", b)
+# Swapping from here
+c = a
+a = b
+print("\nAfter Swap...\n")
+
+print(f"The value in a =", a, "\nThe Value in b =", c)
