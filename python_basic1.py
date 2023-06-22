4f53ece87a7d0dab6b13aca987eebf235db18e15
Akwaduma1997 <134207770+Akwaduma1997@users.noreply.github.com>
2023-06-22 11:42:25 +0100

    Create python_basic1.py

---
+++ python_basic1.py
@@ -0,0 +1,7 @@
+#Program to get user data and print out
+print("Input your Name, Age, PhoneNumber")
+name = input("Enter your name: ")
+age = int(input("Input your age: "))
+phoneNumber = input("Your Phone Number: ")
+
+print("Hello", name, "You're", age, "years old with the phone number", phoneNumber)
